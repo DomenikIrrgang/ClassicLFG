4349c12c1db76844c593c5440c39912f828ab53e
Domenik Irrgang <domenikirrgang@gmail.com>
2019-06-25 11:37:20 +0200

    cleanup of util files and added a size value to dungeons

---
+++ src/util/table.lua
@@ -0,0 +1,18 @@
+function table.inherit(target, base)
+	for key, value in pairs(base) do
+		if (target[key] == nil) then target[key] = value end
+	end
+	target["super"] = base
+	return target
+end
+
+function table.Merge(target, source)
+	for key, value in pairs(source) do
+		if ( istable(value) && istable(target[key])) then
+			table.Merge(target[key], value)
+		else
+			target[key] = value
+		end
+	end
+	return target
+end
--- src/util/util.lua
+++ src/util/util.lua
@@ -6,29 +6,6 @@ function ClassicLFG:IteratorToArray(iterator)
     return array
   end
 
-function string:SplitString(seperator)
-    local fields = {}
-    local pattern = string.format("([^%s]+)", seperator)
-    self:gsub(pattern, function(c) fields[#fields+1] = c end)
-    return fields
-end
-
-function ClassicLFG:SplitString(text, sep)
-    local sep, fields = sep or ":", {}
-    local pattern = string.format("([^%s]+)", sep)
-    text:gsub(pattern, function(c) fields[#fields+1] = c end)
-    return fields
-end
-
-function ClassicLFG:StringContainsTableValue(text, values)
-    for key in pairs(values) do
-        if (string.match(text, values[key]:lower()) ~= nil) then
-            return values[key]
-        end
-    end
-    return nil
-end
-
 function ClassicLFG:SetFrameBackgroundColor(frame, color)
     frame:SetBackdropColor(color.Red, color.Green, color.Blue, color.Alpha)
 end
@@ -88,13 +65,6 @@ function ClassicLFG:ArrayContainsValue(array, val)
     return false
 end
 
-function table.inherit(target, base)
-	for key, value in pairs( base ) do
-		if ( target[ key ] == nil ) then target[ key ] = value end
-	end
-	target[ "super" ] = base
-	return t
-end
 
 function ClassicLFG:IsIgnored(playerName)
     for i = 1, C_FriendList.GetNumIgnores() do
